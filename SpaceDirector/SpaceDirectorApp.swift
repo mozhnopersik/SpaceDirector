3f2a5ab8c8bc1bb9f7dee419fd32fade153613e9
mozhnopersik <mozhnopersik@mail.ru>
2023-11-12 19:41:58 +0300

    Initial Commit

---
+++ SpaceDirector/SpaceDirectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceDirectorApp.swift
+//  SpaceDirector
+//
+//  Created by Вероника Карпова on 12.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct SpaceDirectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
